1ea7c214a22c579e9d84cbe5288b3b9141a0eee1
kam <kam@RedHat.lan>
2026-09-12 01:26:50 +0200

    Remove old stuff

--- database/__init__.py
+++ database/__init__.py
@@ -1,11 +1,3 @@
-"""
-Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized Discord bot in Python
-
-Version: 6.5.0
-"""
-
 import aiosqlite
 
 
